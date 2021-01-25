b8387d4d920c25cc4e8ec73bcc1de743516e6c35
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-01-25 13:52:14 +0100

    Add files via upload

---
+++ ImageList1/ImageList1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ImageList1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pBx1 = new System.Windows.Forms.PictureBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBx1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "slika.jpg");
+            this.imageList1.Images.SetKeyName(1, "pozadina.jpg");
+            // 
+            // pBx1
+            // 
+            this.pBx1.Location = new System.Drawing.Point(84, 12);
+            this.pBx1.Name = "pBx1";
+            this.pBx1.Size = new System.Drawing.Size(618, 317);
+            this.pBx1.TabIndex = 0;
+            this.pBx1.TabStop = false;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(84, 357);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(206, 71);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Promjeni sliku";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pBx1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBx1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pBx1;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
